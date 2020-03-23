a49374c71493998c53a9da92824161b3dbd118e8
gabriel Silva <gabrielsilvacosta15@hotmail.com>
2020-03-23 18:36:07 -0300

    first commit

--- src/pages/Produtos/index.js
+++ src/pages/Produtos/index.js
@@ -1,49 +1,104 @@
 import React, { useEffect, useState } from 'react';
-
+import './style.css';
 import { useParams } from 'react-router-dom';
-import { Nav, Navbar, Row, Col, Container } from 'react-bootstrap';
+import { Nav, Navbar, Row, Col, Container, Card, Button } from 'react-bootstrap';
 
 
 import api from '../../service/api'
 
 export default function Produtos(){
-    const [ categorias, setCategorias ] = useState([]); 
-
+    const [ categorias, setCategorias, setCategoriaEspecifica ] = useState([]); 
+    const [ produtos, setProdutos ] = useState([]);
+    
     let { nomeCategoria } = useParams();
     useEffect(()=>{
-        
+    
         async function loadCategorias(){
             console.log(nomeCategoria, 'nomeCategoria');
             const response = await api.get(`produtos/${nomeCategoria}`); 
-            setCategorias(response.data);
             console.log(response, 'responde');
+            setCategorias(response.data[0].categoria);
+
+            setProdutos(response.data[0].listaProdutosDestaque);
+           
         }
         loadCategorias(nomeCategoria);
     }, []);
     
-    
+    async function loadProdutosDeCategoria(categoriaId){
+        console.log('categoria-enrtou', categoriaId);
+        const response = await api.get(`produtos/${nomeCategoria}`); 
+        console.log(response, 'response-prod');
+    }
+
+    let cardHeaderStyle =  {
+        // backgroundSize: cover,
+        backgroundImage: ``,
+        heigth: 200,
+        // backgroundRepeat: no-repeat,
+
+    }    
     return (
         <>
             <Container>
                 <Row>
-                    <Col lg={4} md={12}>
-                        <h1>Categorias</h1>
-                        <Navbar expand="lg">
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
+                    <Col lg={4} key={1} md={12}>
+                        <Row>
+                            <Col lg={12} md={6} xs>
+                                <h1>Categorias</h1>
+                            </Col>
+
+                            <Col lg={12} md={6} xs>
+                            <Navbar expand="lg">
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
                                 <Nav className="mr-auto links-nav-bar flex-column">
                                     {categorias.map(categoria => (
-                                        <Nav.Link key={ categoria.id } eventKey={ categoria.id }>
-                                            { categoria.nome }
+                                        <Nav.Link key={ categoria.id }>
+                                            <div onClick={ loadProdutosDeCategoria(categoria.id) } >
+                                                { categoria.nome }
+                                            </div>
+                                            
                                         </Nav.Link>
                                     ))}    
                                 </Nav>    
-                            </Navbar.Collapse>
-                        </Navbar>
-                    </Col>
-                    <Col lg={8} md={12}>
+                                </Navbar.Collapse>
+                            </Navbar>
+                            </Col>
 
-                        <h1>prod - {nomeCategoria}</h1>
+                        </Row>
+                        
+                        
+                    </Col>
+                    <Col key={2} lg={8} md={12}>
+                        <Row>
+                            <Col xs={12}><h1>prod - {nomeCategoria}</h1></Col>
+                            
+                                {produtos.map(produto =>(
+                                    <Col key={produto.id} lg={4} md={6} xs>              
+                                        <Card  className="cardProdutos" >
+                                        
+                                            <Card.Header className="cardProdutosHeader" style={{ backgroundImage: `url(${produto.imagem})` }}>
+                                                
+                                            </Card.Header>
+                                            
+                                            <Card.Body>
+                                                <Card.Title>{ produto.nome }</Card.Title>
+                                                <Card.Text>{ produto.descricao }</Card.Text>
+                                                <Button variant="primary">Solicitar orçamento</Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>    
+                                ))}
+
+                            
+                            
+                        </Row>
+                    
+
+                    
+                    
+                        
                     </Col>
 
                 </Row>
